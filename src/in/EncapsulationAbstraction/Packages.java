3e92cc8501dab7308fb042e00e5114af2f674b8f
RAHUL UJJAWAL <vikashkumarjyoti@VIKASHs-MacBook-Air.local>
2026-07-26 20:08:00 +0530

    new add button

--- src/in/EncapsulationAbstraction/Packages.java
+++ src/in/EncapsulationAbstraction/Packages.java
@@ -1,10 +1,7 @@
 package in.EncapsulationAbstraction;
-
-import java.util.Scanner;
-
 public class Packages {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("rahul_ujjawal_sharma");
+        System.out.println("rahul");
+        System.out.println("hey rahul ujjawal");
     }
 }
